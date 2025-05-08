f6ff07b20b57a781d049e8cb96241b9a8d053aab
yutiv <o0533180283@gmail.com>
2025-05-08 14:46:55 +0300

    insert user to mySql

---
+++ chat/backend/chatFront.js
@@ -0,0 +1,9 @@
+const  insertUser=(query)=> {
+    const user={
+         userName:"avi",
+         email:'532555@gmai.com',
+        password:"999"
+    }
+    return user
+}
+module.exports={insertUser}
--- chat/backend/chatServer.js
+++ chat/backend/chatServer.js
@@ -1,13 +1,38 @@
+// // require('dotenv').config();
+// const { SQL_SERVER } = process.env;
+// const sql = require('mssql');
 const express = require('express');
-const app = express();
 
+const app = express();
+var mysql = require('mysql');
+const { insertUser } = require('../backend/chatFront');
 app.listen(3001, function () {
-    console.log('Example app listening on port 3001!');
+  console.log('Example app listening on port 3001!');
+});
+
+var pool = mysql.createConnection({
+  connectionLimit: 10,
+  host: 'localhost',
+  user: 'root',
+  password: 'root111',
+  database: 'chat'
+});
+
+pool.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
 });
 
 app.get('/', function (req, res) {
-    res.send('Hello world!');
-});
-app.get('/get', function (req, res) {
-    res.send('Hello get!');
+  const user = insertUser(req.body)
+  console.log("connect");
+
+  const reqBody = {
+    userName: user.userName,
+    email: user.email,
+    password: user.password
+  }
+  const query = ("INSERT INTO chat.users (userName, email, password) VALUES (?,?,?)")
+  pool.query(query, [reqBody.userName, reqBody.email, reqBody.password]);
+  res.send(res);
 });
