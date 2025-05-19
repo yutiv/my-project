e05140a61b7e774fde602a9bdf08806bb1effbca
yutiv <o0533180283@gmail.com>
2025-05-19 19:06:46 +0300

    finish login

--- chat/backend/routes/userRouter.js
+++ chat/backend/routes/userRouter.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { getUsers, createUser } = require('../controllers/userController');
+const { getUsers, createUser, login } = require('../controllers/userController');
 const router = express.Router();
 
 router.get('/', getUsers);
 router.post('/', createUser);
+router.post('/login', login);
 
 module.exports = router;
